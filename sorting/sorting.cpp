2197b3fecfe87f15c606667dc48c8dc8775532b9
ka-karot <anshuayan1@gmail.com>
2025-08-20 00:07:29 +0530

    new beginning?

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -1,3 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// MERGE SORT  |  // O(n^2)  - worst/avg  |  O(n) - best
+// Merge sort is a sorting algorithm that repeatedly divides the unsorted array into two halves, sorts each half recursively, and then merges the sorted halves to produce the final sorted list.
+
+void mergeSort(){
+    
+}
