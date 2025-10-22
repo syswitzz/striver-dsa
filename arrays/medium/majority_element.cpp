2e4b0cdf810ba7f5e2485bc8cc91684b9ecc3b6d
syswitzz <132119462+syswitzz@users.noreply.github.com>
2025-10-22 19:29:48 +0530

    majority elememnt

---
+++ arrays/medium/majority_element.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+// MAJORITY ELEMENT (>N/2) (better) |  O(nlogn + n)  |  O(n)
+int majorityElement(vector<int> arr){
+    map<int, int> hashmap; // O(n)
+
+    for(int i=0; i<arr.size(); i++){ //O(nlogn)
+        hashmap[arr[i]]++;
+    }
+    for(auto it: hashmap){  //O(n)
+        if(it.second>arr.size()/2) return it.first;
+    }
+    return 0;
+}
+
+// MAJORITY ELEMENT (>N/2) (optimal) |  O(nlogn + n)  |  O(n)
+// MOOSE'S VOTING ALGORITHM
+int majorityElementOptimal(vector<int> arr){
+    int element;
+    int count = 0;
+
+    for (int i=0; i<arr.size(); i++){
+        if(count==0){
+            count =1;
+            element= arr[i];
+        }
+        else if (arr[i]==element){
+            count++;
+        }
+        else count--;
+    }
+
+    // extra verification steps
+    int countt=0;
+    for(int i=0; i<arr.size(); i++) if(arr[i]==element) countt++;
+    
+    if(countt>arr.size()/2) return element;
+    else return 0;
+}
+
+
+int main(){
+    vector<int> arr = {2,3,5,5,5,5,3};
+    int me = majorityElementOptimal(arr);
+
+    cout << me;
+    return 0;
+}
