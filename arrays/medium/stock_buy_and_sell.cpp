d346f426f4793e65e2f831e88a437a06fd7ab0ec
syswitzz <132119462+syswitzz@users.noreply.github.com>
2025-10-25 14:26:51 +0530

    changes

--- arrays/medium/stock_buy_and_sell.cpp
+++ arrays/medium/stock_buy_and_sell.cpp
@@ -4,22 +4,21 @@ using namespace std;
 
 
 // STOCK BUY AND SELL  |  O(n)  |  O(1)
-int stock(vector<int> arr){
-    int buy = arr[0];
-    int max_profit = 0;
-
-    for(int i=1; i<arr.size(); i++){
-        int profit = arr[i]-buy;
-        max_profit = max(max_profit, profit);
-        buy = min(buy, arr[i]);
+int maxProfit(vector<int>& prices) {
+    int max=INT_MIN;
+    int min=prices[0];
+    for (int i=1;i<prices.size();i++){
+        int profit=prices[i]-min;
+        if (profit>max) max=profit;
+        if (prices[i]<min) min=prices[i];
     }
-
-    return max_profit;
-}
+    if (max>0) return max;
+    return 0; 
+}  
 
 int main(){
     vector<int> arr = {2,3,5,5,5,5,3};
-    int me = stock(arr);
+    int me = maxProfit(arr);
 
     cout << me;
     return 0;
